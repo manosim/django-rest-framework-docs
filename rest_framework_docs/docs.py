39f78bd6539cc7cc7cebb15670187f9995a28bb3
Marc Gibbons <marc_gibbons@rogers.com>
2013-04-01 00:09:58 -0400

    Replaced PEP-257 trim code with djangoadmin docs

--- rest_framework_docs/docs.py
+++ rest_framework_docs/docs.py
@@ -1,8 +1,8 @@
 import jsonpickle
-import sys
 import re
 from django.conf import settings
 from django.utils.importlib import import_module
+from django.contrib.admindocs.utils import trim_docstring
 from rest_framework.views import APIView
 from django.core.urlresolvers import RegexURLResolver, RegexURLPattern
 from itertools import groupby
@@ -236,33 +236,10 @@ class DocumentationGenerator():
 
     def __trim(self, docstring):
         """
-        Trims whitespace from the docstring in accordance to the PEP-257 standard
-        From: http://www.python.org/dev/peps/pep-0257/#multi-line-docstrings
+        Trims whitespace from docstring
         """
-        if not docstring:
-            return ''
-        # Convert tabs to spaces (following the normal Python rules)
-        # and split into a list of lines:
-        lines = docstring.expandtabs().splitlines()
-        # Determine minimum indentation (first line doesn't count):
-        indent = sys.maxint
-        for line in lines[1:]:
-            stripped = line.lstrip()
-            if stripped:
-                indent = min(indent, len(line) - len(stripped))
-        # Remove indentation (first line is special):
-        trimmed = [lines[0].strip()]
-        if indent < sys.maxint:
-            for line in lines[1:]:
-                trimmed.append(line[indent:].rstrip())
-        # Strip off trailing and leading blank lines:
-        while trimmed and not trimmed[-1]:
-            trimmed.pop()
-        while trimmed and not trimmed[0]:
-            trimmed.pop(0)
-        # Return a single string:
-        return '\n'.join(trimmed)
-
+        return trim_docstring(docstring)
+#
     class ApiDocObject(object):
         """ API Documentation Object """
         path = None
