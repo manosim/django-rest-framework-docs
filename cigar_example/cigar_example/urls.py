874f01ad4de31da06bebe9011a35e6bba6187875
Marc Gibbons <marc.gibbons@bnotions.com>
2013-04-03 12:46:38 -0400

    Changed example URL pattern to use a prefix when importing api urls

--- cigar_example/cigar_example/urls.py
+++ cigar_example/cigar_example/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, include
 
 urlpatterns = patterns('',
-    (r'', include('cigar_example.restapi.urls')),
+    (r'api/', include('cigar_example.restapi.urls')),
     (r'', include('cigar_example.app.urls')),
 )
