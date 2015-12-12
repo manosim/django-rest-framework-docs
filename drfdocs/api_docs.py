8bfbd0a8d87f4297ad421176cd7dac046bf6f484
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-12 13:54:30 +0000

    Exclude "serve" view (static files)

--- drfdocs/api_docs.py
+++ drfdocs/api_docs.py
@@ -4,6 +4,7 @@ from django.core.urlresolvers import RegexURLResolver, RegexURLPattern
 
 class ApiDocumentation(object):
     excluded_apps = ["admin", "drfdocs"]
+    excluded_endpoints = ["serve"]
     root_urlconf = __import__(settings.ROOT_URLCONF)
 
     def __init__(self):
@@ -14,7 +15,7 @@ class ApiDocumentation(object):
         for pattern in urlpatterns:
             if isinstance(pattern, RegexURLResolver) and (pattern.app_name not in self.excluded_apps):
                 self.get_all_view_names(pattern.url_patterns)
-            elif isinstance(pattern, RegexURLPattern):
+            elif isinstance(pattern, RegexURLPattern) and (pattern.callback.__name__ not in self.excluded_endpoints):
                 self.view_names.append(pattern.callback.__name__)
 
     def get_views(self):
