0dab37e88953563f1d6ce4c6becf09518f154823
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-12 21:54:18 +0000

    List urls with namespaces' urls (parent patterns)

--- drfdocs/api_docs.py
+++ drfdocs/api_docs.py
@@ -12,13 +12,17 @@ class ApiDocumentation(object):
         root_urlconf = __import__(settings.ROOT_URLCONF)
         self.get_all_view_names(root_urlconf.urls.urlpatterns)
 
-    def get_all_view_names(self, urlpatterns):
+    def get_all_view_names(self, urlpatterns, parent_pattern=None):
         for pattern in urlpatterns:
             if isinstance(pattern, RegexURLResolver) and (pattern.app_name not in self.excluded_apps):
-                self.get_all_view_names(pattern.url_patterns)
+                self.get_all_view_names(urlpatterns=pattern.url_patterns, parent_pattern=pattern)
             elif isinstance(pattern, RegexURLPattern) and (pattern.callback.__name__ not in self.excluded_endpoints):
-                api_endpoint = ApiEndpoint(pattern)
+                api_endpoint = ApiEndpoint(pattern, parent_pattern)
                 self.endpoints.append(api_endpoint)
+
+    def _filter_drf_views(self, endpoints):
+        # Should keep only the endpoints with views that inherit from DRF's APIView
+        pass
 
     def get_endpoints(self):
         return self.endpoints
--- drfdocs/api_endpoint.py
+++ drfdocs/api_endpoint.py
@@ -4,8 +4,11 @@ from rest_framework.views import APIView
 
 class ApiEndpoint(object):
 
-    def __init__(self, pattern):
+    def __init__(self, pattern, parent_pattern=None):
         self.pattern = pattern
+        self.url_parent_regex = simplify_regex(parent_pattern.regex.pattern)[:-1] if parent_pattern else None
+        self.url_regex = ("{0}{1}".format(self.url_parent_regex, simplify_regex(pattern.regex.pattern))) if self.url_parent_regex else simplify_regex(pattern.regex.pattern)
+        self.url_name = pattern.name
         self.regex = simplify_regex(pattern._regex)
         self.view_name = pattern.callback.__name__
         # self._get_api_callback(pattern)
