370ce895022afdd157532425fbef34ae41e4417c
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-12 22:15:26 +0000

    Check if each view inherits from DRF's ApiView

--- drfdocs/api_docs.py
+++ drfdocs/api_docs.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.urlresolvers import RegexURLResolver, RegexURLPattern
 from drfdocs.api_endpoint import ApiEndpoint
+from rest_framework.views import APIView
 
 
 class ApiDocumentation(object):
@@ -16,13 +17,15 @@ class ApiDocumentation(object):
         for pattern in urlpatterns:
             if isinstance(pattern, RegexURLResolver) and (pattern.app_name not in self.excluded_apps):
                 self.get_all_view_names(urlpatterns=pattern.url_patterns, parent_pattern=pattern)
-            elif isinstance(pattern, RegexURLPattern) and (pattern.callback.__name__ not in self.excluded_endpoints):
+            elif isinstance(pattern, RegexURLPattern) and (pattern.callback.__name__ not in self.excluded_endpoints) and self._is_drf_view(pattern):
                 api_endpoint = ApiEndpoint(pattern, parent_pattern)
                 self.endpoints.append(api_endpoint)
 
-    def _filter_drf_views(self, endpoints):
-        # Should keep only the endpoints with views that inherit from DRF's APIView
-        pass
+    def _is_drf_view(self, pattern):
+        # Should check whether a pattern inherits from DRF's APIView
+        if (hasattr(pattern.callback, 'cls') and issubclass(pattern.callback.cls, APIView)):
+            return True
+        return False
 
     def get_endpoints(self):
         return self.endpoints
