245d63ae68addd2908f6a2875c82d83be9b91deb
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-13 00:59:52 +0000

    Get list of methods

--- drfdocs/api_endpoint.py
+++ drfdocs/api_endpoint.py
@@ -5,12 +5,17 @@ class ApiEndpoint(object):
 
     def __init__(self, pattern, parent_pattern=None):
         self.pattern = pattern
+        self.callback = pattern.callback
         self.name = pattern.name
-        self.path = self._get_path(parent_pattern)
-        self.view_name = pattern.callback.__name__
+        self.path = self.__get_path__(parent_pattern)
+        self.allowed_methods = self.__get_allowed_methods__()
+        # self.view_name = pattern.callback.__name__
 
-    def _get_path(self, parent_pattern):
+    def __get_path__(self, parent_pattern):
         if parent_pattern:
             parent_path = simplify_regex(parent_pattern.regex.pattern)[:-1]
             return "{0}{1}".format(parent_path, simplify_regex(self.pattern.regex.pattern))
         return simplify_regex(self.pattern.regex.pattern)
+
+    def __get_allowed_methods__(self):
+        return [m.upper() for m in self.callback.cls.http_method_names if hasattr(self.callback.cls, m)]
