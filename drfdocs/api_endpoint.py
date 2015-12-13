fea681a2d0b99f7d66d58cbc91cb403dea428e09
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-13 14:44:58 +0000

    Move CSS to LESS

--- drfdocs/api_endpoint.py
+++ drfdocs/api_endpoint.py
@@ -6,11 +6,11 @@ class ApiEndpoint(object):
     def __init__(self, pattern, parent_pattern=None):
         self.pattern = pattern
         self.callback = pattern.callback
-        self.name = pattern.name
+        # self.name = pattern.name
         self.name_parent = simplify_regex(parent_pattern.regex.pattern).replace('/', '') if parent_pattern else None
         self.path = self.__get_path__(parent_pattern)
         self.allowed_methods = self.__get_allowed_methods__()
-        self.view_name = pattern.callback.__name__
+        # self.view_name = pattern.callback.__name__
         self.fields = self.__get_serializer_fields__()
 
     def __get_path__(self, parent_pattern):
