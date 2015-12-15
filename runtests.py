63852912c49d3f33f16be6e20c999788392561ff
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-15 23:24:59 +0000

    Working tests

--- runtests.py
+++ runtests.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+import os
 import sys
 import subprocess
+import django
+from django.conf import settings
+from django.test.utils import get_runner
 
 
-FLAKE8_ARGS = ['demo/project/', 'rest_framework_docs', '--ignore=E501']
+FLAKE8_ARGS = ['demo/project/', 'rest_framework_docs', 'tests/', '--ignore=E501']
 
 
 def exit_on_failure(command, message=None):
@@ -17,4 +21,16 @@ def flake8_main(args):
     print("" if command else "Success. flake8 passed.")
     return command
 
+
+def django_tests_main():
+    if __name__ == "__main__":
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+        django.setup()
+        TestRunner = get_runner(settings)
+        test_runner = TestRunner()
+        failures = test_runner.run_tests(["tests"])
+        sys.exit(bool(failures))
+
 exit_on_failure(flake8_main(FLAKE8_ARGS))
+
+django_tests_main()
--- tests/settings.py
+++ tests/settings.py
@@ -1,4 +1,18 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 SECRET_KEY = 'django-rest-framework-docs-key'
+
+# Database
+# https://docs.djangoproject.com/en/1.8/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 INSTALLED_APPS = [
     "rest_framework",
