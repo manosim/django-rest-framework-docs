1d6cfda3fbd9f3bdb890584b4aba4d4fd13c9743
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-16 15:07:53 +0000

    Setup Tests with Coverage

--- runtests.py
+++ runtests.py
@@ -3,6 +3,7 @@ import os
 import sys
 import subprocess
 import django
+from coverage import coverage
 from django.conf import settings
 from django.test.utils import get_runner
 
@@ -22,15 +23,28 @@ def flake8_main(args):
     return command
 
 
-def django_tests_main():
+def run_tests_coverage():
     if __name__ == "__main__":
         os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
         django.setup()
         TestRunner = get_runner(settings)
         test_runner = TestRunner()
+
+        # Setup Coverage
+        cov = coverage(source=["rest_framework_docs"], omit=["__init__.py"])
+        cov.start()
+
         failures = test_runner.run_tests(["tests"])
-        sys.exit(bool(failures))
+
+        if bool(failures):
+            cov.erase()
+            sys.exit("Tests Failed. Coverage Cancelled.")
+
+        # If success show coverage results
+        cov.stop()
+        cov.save()
+        cov.report()
+        # cov.html_report(directory='covhtml')
 
 exit_on_failure(flake8_main(FLAKE8_ARGS))
-
-django_tests_main()
+exit_on_failure(run_tests_coverage())
