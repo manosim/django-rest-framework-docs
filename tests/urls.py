7f984f5c4b62f8c2373808c0180b0f694b431a78
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-16 17:44:52 +0000

    Setup Urls and sample view

--- tests/urls.py
+++ tests/urls.py
@@ -1,11 +1,20 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from tests import views
+
+accounts_urls = [
+    url(r'^login/$', views.LoginView.as_view(), name="login"),
+]
+
+organisations_urls = [
+
+]
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^docs/', include('rest_framework_docs.urls')),
 
     # API
-    # url(r'^accounts/', view=include('project.accounts.urls', namespace='accounts')),
-    # url(r'^organisations/', view=include('project.organisations.urls', namespace='organisations')),
+    url(r'^accounts/', view=include(accounts_urls, namespace='accounts')),
+    url(r'^organisations/', view=include(organisations_urls, namespace='organisations')),
 ]
---
+++ tests/views.py
@@ -0,0 +1,29 @@
+from django.views.generic.base import TemplateView
+from rest_framework import parsers, renderers
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.serializers import AuthTokenSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class TestView(TemplateView):
+    """
+    This view should not be included in DRF Docs.
+    """
+    template_name = "a_test.html"
+
+
+class LoginView(APIView):
+
+    throttle_classes = ()
+    permission_classes = ()
+    parser_classes = (parsers.FormParser, parsers.MultiPartParser, parsers.JSONParser,)
+    renderer_classes = (renderers.JSONRenderer,)
+    serializer_class = AuthTokenSerializer
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
