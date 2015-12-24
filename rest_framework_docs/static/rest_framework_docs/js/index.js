dad8217017df99e611b85ffc25bef0cd33b19d2d
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-24 16:26:13 +0200

    Simpler JSON pretty print

--- rest_framework_docs/static/rest_framework_docs/js/index.js
+++ rest_framework_docs/static/rest_framework_docs/js/index.js
@@ -1,3 +1,25 @@
+var jsonPP = {
+  // Thanks to http://jsfiddle.net/unlsj/
+  replacer: function (match, pIndent, pKey, pVal, pEnd) {
+     var key = '<span class=json-key>';
+     var val = '<span class=json-value>';
+     var str = '<span class=json-string>';
+     var r = pIndent || '';
+     if (pKey)
+        r = r + key + pKey.replace(/[": ]/g, '') + '</span>: ';
+     if (pVal)
+        r = r + (pVal[0] == '"' ? str : val) + pVal + '</span>';
+     return r + (pEnd || '');
+  },
+  prettyPrint: function (obj) {
+     var jsonLine = /^( *)("[\w]+": )?("[^"]*"|[\w.+-]*)?([,[{])?$/mg;
+     return JSON.stringify(obj, null, 3)
+        .replace(/&/g, '&amp;').replace(/\\"/g, '&quot;')
+        .replace(/</g, '&lt;').replace(/>/g, '&gt;')
+        .replace(jsonLine, this.replacer);
+  }
+};
+
 $( document ).ready(function() {
 
   var setResponse = function (response) {
@@ -28,7 +50,7 @@ $( document ).ready(function() {
     $('#responseStatusCode').addClass(statusCodeClass);
 
     $('#responseStatusText').text(response.statusText.toLowerCase());
-    $('#responseData').JSONView(response.responseJSON);
+    $('#responseData').html(jsonPP.prettyPrint(response.responseJSON));
 
     // console.log(response);
     // console.log(response.responseJSON);
