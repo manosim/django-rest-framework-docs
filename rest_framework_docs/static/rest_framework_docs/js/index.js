d5ac99a94376b7bdc681f7d59e2b45cf9508da92
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-24 04:09:06 +0200

    Init Making the Request & Getting the response

--- rest_framework_docs/static/rest_framework_docs/js/index.js
+++ rest_framework_docs/static/rest_framework_docs/js/index.js
@@ -1,5 +1,54 @@
 $( document ).ready(function() {
 
+  var makeRequest = function () {
+    var url = $('#requestForm #urlInput').val();
+    var method = 'POST';
 
+    $.ajax({
+      url: url,
+      method: method,
+      context: document.body,
+      data: {
+        username: 'test',
+        password: 'test'
+      }
+    }).always(function(response) {
+
+      $('#responseStatusCode').text(response.status);
+      $('#responseStatusText').text(response.statusText);
+      $('#responseData').text(JSON.stringify(response.responseJSON));
+
+      console.log(response);
+      console.log(response.responseJSON);
+    });
+  };
+
+  var setupForm = function (data) {
+    console.log('------');
+    console.log(data.path);
+    console.log('------');
+
+    $('#urlInput').val(data.path);
+
+    $('#requestForm').submit(function (e) {
+      // Prevent Submit
+      e.preventDefault();
+
+      // Make Request
+      makeRequest(data);
+    });
+  };
+
+  $('.plug').bind('click', function(e) {
+    // Prevent the accordion from collapsing
+    e.stopPropagation();
+
+    // Open Modal
+    $('#liveAPIModal').modal('toggle');
+
+    // Setup the form
+    var data = $(this).data();
+    setupForm(data);
+  });
 
 });
