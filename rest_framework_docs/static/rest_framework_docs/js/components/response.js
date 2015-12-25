b9360d89b8568f1a0e6a811174fef835f657d59f
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-25 15:57:24 +0200

    Make the request

--- rest_framework_docs/static/rest_framework_docs/js/components/response.js
+++ rest_framework_docs/static/rest_framework_docs/js/components/response.js
@@ -1,6 +1,8 @@
 var _ = require('underscore');
 var React = require('react');
 
+var JSONpp = require('../utils/jsonpp');
+
 var Response = React.createClass({
 
   getInitialState: function () {
@@ -10,12 +12,26 @@ var Response = React.createClass({
   },
 
   render: function () {
+    if (!this.props.payload) {
+      return (
+        <div>
+          <h3>Response <span className="label status-code pull-right"></span></h3>
+          <p className="lead text-center">Awaiting request...</p>
+        </div>
+      )
+    }
+
+    var responseJSON = JSONpp.prettyPrint(this.props.payload.body);
+    var statusText = this.props.payload.statusText.toLowerCase();
+    var statusCodeFirstChar = String(this.props.payload.status).charAt(0);
+    var statusCodeClass = "label status-code pull-right status-code-" + statusCodeFirstChar;
+
     return (
       <div>
-        <h3>Response <span className="label status-code pull-right"></span></h3>
+        <h3>Response <span className={statusCodeClass}>{this.props.payload.status}</span></h3>
 
-        <div><strong>Status</strong>: <span className="status-text"></span></div>
-        <pre><code></code></pre>
+        <div><strong>Status</strong>: <span className="status-text">{statusText}</span></div>
+        <pre><code dangerouslySetInnerHTML={{__html: responseJSON}}></code></pre>
         <div className="well well-default text-center">
           <button className='btn btn-info'><i className='fa fa-key'></i> Save Token</button>
           <h5>Your token will be lost when you refresh the page.</h5>
