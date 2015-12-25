5dea7f879c267dbaa6a663faf8f3a85014da13e8
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2015-12-25 15:05:02 +0200

    List Fields

--- rest_framework_docs/static/rest_framework_docs/js/components/request/fields-data.js
+++ rest_framework_docs/static/rest_framework_docs/js/components/request/fields-data.js
@@ -1,24 +1,25 @@
-var _ = require('underscore');
 var React = require('react');
 
 var Input = require('../helpers/input');
 
 var FieldsData = React.createClass({
 
-  handleChange: function (value) {
-    this.props.onChange(value);
+  handleChange: function (value, event) {
+    this.props.onChange(value, event);
   },
 
   _renderFields: function () {
-    return _.each(this.props.fields, function (field, key) {
-      console.log('======');
-      console.log(key);
-      console.log(field);
-
+    return this.props.fields.map(function (field, key) {
       return (
-        <Input key={key} name={field.name} value='' placeholder={field.type} required={field.required} onChange={this.handleChange} />
+        <Input
+          key={key}
+          name={field.name}
+          value=''
+          placeholder={field.type}
+          required={field.required}
+          onChange={this.handleChange.bind(this, field.name)} />
       );
-    }, this )
+    }, this)
   },
 
   render: function () {
