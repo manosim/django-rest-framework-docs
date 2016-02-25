9bbb6b643f9e3a2655742c89de6ed2cea0d05d84
Emmanouil Konstantinidis <manos@iamemmanouil.com>
2016-02-25 21:47:04 +0000

    Exclude Endpoints with a <format> parameter

--- rest_framework_docs/api_docs.py
+++ rest_framework_docs/api_docs.py
@@ -20,13 +20,21 @@ class ApiDocumentation(object):
             if isinstance(pattern, RegexURLResolver):
                 parent_pattern = None if pattern._regex == "^" else pattern
                 self.get_all_view_names(urlpatterns=pattern.url_patterns, parent_pattern=parent_pattern)
-            elif isinstance(pattern, RegexURLPattern) and self._is_drf_view(pattern):
+            elif isinstance(pattern, RegexURLPattern) and self._is_drf_view(pattern) and not self._is_format_endpoint(pattern):
                 api_endpoint = ApiEndpoint(pattern, parent_pattern)
                 self.endpoints.append(api_endpoint)
 
     def _is_drf_view(self, pattern):
-        # Should check whether a pattern inherits from DRF's APIView
+        """
+        Should check whether a pattern inherits from DRF's APIView
+        """
         return hasattr(pattern.callback, 'cls') and issubclass(pattern.callback.cls, APIView)
+
+    def _is_format_endpoint(self, pattern):
+        """
+        Exclude endpoints with a "format" parameter
+        """
+        return '?P<format>' in pattern._regex
 
     def get_endpoints(self):
         return self.endpoints
