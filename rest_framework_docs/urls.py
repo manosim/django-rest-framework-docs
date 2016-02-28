6f1ac221802a970452426b801e460680d7b9b81c
Nagkumar <me@nagkumar.com>
2016-02-27 18:59:49 -0800

    Added the login required mixin to the view so that logged in users can view the docs.

--- rest_framework_docs/urls.py
+++ rest_framework_docs/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import url
 from rest_framework_docs.views import DRFDocsView
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     # Url to view the API Docs
-    url(r'^$', DRFDocsView.as_view(), name='drfdocs'),
+    url(r'^$', login_required(DRFDocsView.as_view()), name='drfdocs'),
 ]
