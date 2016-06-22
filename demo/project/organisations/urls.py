935a3848eb684f7982ce838e972a7bcda4ac5699
Ivaylo Bachvarof <bachvarof@gmail.com>
2016-06-22 21:15:27 +0300

    Add nested serializers support

--- demo/project/organisations/urls.py
+++ demo/project/organisations/urls.py
@@ -5,6 +5,7 @@ from project.organisations import views
 urlpatterns = [
 
     url(r'^create/$', view=views.CreateOrganisationView.as_view(), name="create"),
+    url(r'^(?P<slug>[\w-]+)/$', view=views.RetrieveOrganisationView.as_view(), name="organisation"),
     url(r'^(?P<slug>[\w-]+)/members/$', view=views.OrganisationMembersView.as_view(), name="members"),
     url(r'^(?P<slug>[\w-]+)/leave/$', view=views.LeaveOrganisationView.as_view(), name="leave")
 
