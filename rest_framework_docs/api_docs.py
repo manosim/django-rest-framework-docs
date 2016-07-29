a3c7ffb6cb05fe91fe3cd9cb6e2002e44858f437
Radek Czajka <rczajka@rczajka.pl>
2016-07-29 16:41:39 +0200

    Bugfix: an endpoint without a parent contaminated with its predecessor's parent.

--- rest_framework_docs/api_docs.py
+++ rest_framework_docs/api_docs.py
@@ -24,8 +24,8 @@ class ApiDocumentation(object):
     def get_all_view_names(self, urlpatterns, parent_pattern=None):
         for pattern in urlpatterns:
             if isinstance(pattern, RegexURLResolver):
-                parent_pattern = None if pattern._regex == "^" else pattern
-                self.get_all_view_names(urlpatterns=pattern.url_patterns, parent_pattern=parent_pattern)
+                new_parent_pattern = None if pattern._regex == "^" else pattern
+                self.get_all_view_names(urlpatterns=pattern.url_patterns, parent_pattern=new_parent_pattern)
             elif isinstance(pattern, RegexURLPattern) and self._is_drf_view(pattern) and not self._is_format_endpoint(pattern):
                 api_endpoint = ApiEndpoint(pattern, parent_pattern, self.drf_router)
                 self.endpoints.append(api_endpoint)
